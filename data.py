8cc0e553b6d262c39ec3cb2ccdbabd35b1b6b8e2
Sheema Parwaz <SheemaParwaz@gmail.com>
2022-01-18 12:44:44 +0530

    debugged

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import csv
 
 df = pd.read_csv('data.csv')
 
-xValue = df.groupby(['student_id',"level"])["attempt"].mean()
+xValue = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
 
 fig = px.scatter( xValue, x ='student_id', y = 'level', size ="attempt", color = "attempt")
 
